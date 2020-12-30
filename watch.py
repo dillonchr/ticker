300deea9fdeaf1383cdbe75e7f77d4d0e2d06d70
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-12-30 07:41:27 -0600

    updated to ditch xrp

--- watch.py
+++ watch.py
@@ -1,11 +1,12 @@
 import time
 import datetime
 import os
+import json
 
 import requests
 from pydash import get
 
-URL = "https://api.coingecko.com/api/v3/simple/price?ids=litecoin%2Cethereum%2Cripple&vs_currencies=usd"
+URL = "https://api.coingecko.com/api/v3/simple/price?ids=litecoin%2Cethereum%2Cbitcoin&vs_currencies=usd"
 
 def get_prices():
     with requests.get(URL) as r:
@@ -17,16 +18,23 @@ def should_buy():
     global last_update
     prices = get_prices()
     ltc = get(prices, "litecoin.usd", 0)
-    ripple = get(prices, "ripple.usd", 0)
+    btc = get(prices, "bitcoin.usd", 0)
     eth = get(prices, "ethereum.usd", 0)
-    if 0.39 < ripple and eth < 621:
+    if 137 < ltc:
         say_update("Oh my god. You have to trade now!")
-        print("YOU SHOULD BUY NOW")
     else:
-        update = f"LTC ${ltc} ETH ${eth} XRP ${ripple}"
+        update = f"LTC ${ltc} ETH ${eth} BTC ${btc}"
         if last_update != update:
             last_update = update
             print(f"{update} {datetime.datetime.now()}")
+            shares = get_shares()
+            value = shares.get("eth", 0) * eth + shares.get("btc", 0) * btc + shares.get("ltc", 0) * ltc
+            print(f"portfolio total: {value}")
+
+def get_shares():
+    with open("shares.json", "r") as share_data:
+        data = share_data.read()
+        return json.loads(data)
 
 def say_update(update):
     os.system(f"say \"{update}\"")
